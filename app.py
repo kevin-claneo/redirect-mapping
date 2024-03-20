bbe18ceb911013364c7f43a19c0e4f1e653fb0b0
kevin-claneo <108477383+kevin-claneo@users.noreply.github.com>
2024-03-20 16:15:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,14 @@ from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
 from huggingface_hub import HfFolder
-from stqdm import stqdm
-
-st.set_page_config(
-    page_title="AI Driven Redirect Mapping",
-    page_icon=":arrow_right_hook:",
-    layout="wide",
-    menu_items={
-        'Get Help': 'https://www.linkedin.com/in/kirchhoff-kevin/',
-        'About': "This is an app for finding the matching redirect URLs using the FAISS model."
-    }
-)
 
 def main():
-    st.image("https://www.claneo.com/wp-content/uploads/Element-4.svg", width=600, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-    st.caption(":point_right: Join Claneo and support exciting clients as part of the Consulting team") 
-    st.caption(':bulb: Make sure to mention that *Kevin* brought this job posting to your attention')
-    st.link_button("Learn More", "https://www.claneo.com/en/career/#:~:text=Consulting")
-    st.title("Use Facebook AI Similarity Search (Faiss) to find matching redirect URLs")
+    st.title("Site Migration Redirects Automator v.1")
     st.markdown("""
-    This tool is based on the original Python script [Automated Redirect Matchmaker for Site Migrations](https://colab.research.google.com/drive/1Y4msGtQf44IRzCotz8KMy0oawwZ2yIbT?usp=sharing) developed by [Daniel Emery](https://www.linkedin.com/in/dpe1/).
+    ##### Before Using the Tool
     
-    ##### Before Using the Tool 
     To ensure the effectiveness of this tool in mapping redirects, it is essential to adequately prepare the input data. This process begins with exporting data from *Screaming Frog*.
-   
+    
     ##### 👉🏼 Prepare Data with Screaming Frog
     
     1. Run a full crawl of your website using Screaming Frog.
@@ -41,13 +25,17 @@ def main():
     2. Upload the CSV files using the provided uploaders.
     3. Select the relevant columns for matching from the dropdown menu.
     4. Click the "Match URLs" button to start the matching process.
-    5. Download the results via "Download Results".
+    5. Download the results directly from the interface, which will include the origin URLs, the corresponding destination URLs, and the similarity matching.
+    
+    ###### Credits
+    
+    This tool is based on the original Python script [Automated Redirect Matchmaker for Site Migrations](https://colab.research.google.com/drive/1Y4msGtQf44IRzCotz8KMy0oawwZ2yIbT?usp=sharing) developed by [Daniel Emery](https://www.linkedin.com/in/dpe1/), which provides an automated approach to mapping redirects during website migrations. The tool has been extended and integrated into a Streamlit application to improve interactivity and usability.
     """)
     
     st.markdown("---")
-
+    
     # Field for entering the Hugging Face token
-    hf_token = str(st.secrets["installed"]["hf_token"])
+    hf_token = "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"
     
     if hf_token:
         # Set the Hugging Face token
@@ -60,38 +48,31 @@ def main():
     if origin_file and destination_file:
         origin_df = pd.read_csv(origin_file)
         destination_df = pd.read_csv(destination_file)
-    
+
         # Identification of common columns
         common_columns = list(set(origin_df.columns) & set(destination_df.columns))
         selected_columns = st.multiselect("Select columns to use for similarity matching:", common_columns)
-    
+
         if st.button("Match URLs") and selected_columns:
             # Preprocessing of data
             origin_df['combined_text'] = origin_df[selected_columns].apply(lambda row: ' '.join(row.values.astype(str)), axis=1)
             destination_df['combined_text'] = destination_df[selected_columns].apply(lambda row: ' '.join(row.values.astype(str)), axis=1)
-    
+
             # Matching of data
             model = SentenceTransformer('all-MiniLM-L6-v2')
-    
-            # Initialize progress bars
-            progress_bar_origin = st.progress(0.0)
-            progress_bar_destination = st.progress(0.0)
-    
-            # Use stqdm to wrap the loop for real-time progress updates for origin texts
-            for i in stqdm(range(len(origin_df)), desc="Encoding origin texts"):
-                origin_embeddings = model.encode(origin_df['combined_text'].iloc[i:i+1].tolist(), show_progress_bar=False)
-                progress_value = (i + 1) / len(origin_df)
-                progress_bar_origin.progress(progress_value)
-    
-            # Use stqdm to wrap the loop for real-time progress updates for destination texts
-            for i in stqdm(range(len(destination_df)), desc="Encoding destination texts"):
-                destination_embeddings = model.encode(destination_df['combined_text'].iloc[i:i+1].tolist(), show_progress_bar=False)
-                progress_value = (i + 1) / len(destination_df)
-                progress_bar_destination.progress(progress_value)
-                
-                # Creation of series to handle different lengths
-                matched_url_series = pd.Series(destination_df['Address'].iloc[indices.flatten()].values, index=origin_df.index)
-                similarity_scores_series = pd.Series(similarity_scores.flatten(), index=origin_df.index)
+            origin_embeddings = model.encode(origin_df['combined_text'].tolist(), show_progress_bar=True)
+            destination_embeddings = model.encode(destination_df['combined_text'].tolist(), show_progress_bar=True)
+
+            dimension = origin_embeddings.shape[1]
+            faiss_index = faiss.IndexFlatL2(dimension)
+            faiss_index.add(destination_embeddings.astype('float32'))
+
+            distances, indices = faiss_index.search(origin_embeddings.astype('float32'), k=1)
+            similarity_scores = 1 - (distances / np.max(distances))
+
+            # Creation of series to handle different lengths
+            matched_url_series = pd.Series(destination_df['Address'].iloc[indices.flatten()].values, index=origin_df.index)
+            similarity_scores_series = pd.Series(similarity_scores.flatten(), index=origin_df.index)
 
             # Creation of the results DataFrame
             results_df = pd.DataFrame({
@@ -104,13 +85,12 @@ def main():
             csv_string = results_df.to_csv(index=False)
 
             # Display download button
-            if st.button("Download Results"):
-                st.download_button(
-                    label="Download Results",
-                    data=csv_string,
-                    file_name='redirect_mapping_results.csv',
-                    mime='text/csv'
-                )
+            st.download_button(
+                label="Download Results",
+                data=csv_string,
+                file_name='redirect_mapping_results.csv',
+                mime='text/csv'
+            )
 
 if __name__ == "__main__":
     main()
