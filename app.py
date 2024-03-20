9e78acaa005fb9b2fde00a0549f77673576d022d
kevin-claneo <108477383+kevin-claneo@users.noreply.github.com>
2024-03-20 14:08:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from stqdm import stqdm
 st.set_page_config(
     page_title="AI Driven Redirect Mapping",
     page_icon=":arrow_right_hook:",
-    layout="wide",
     menu_items={
         'Get Help': 'https://www.linkedin.com/in/kirchhoff-kevin/',
         'About': "This is an app for finding the matching redirect URLs using the FAISS model."
