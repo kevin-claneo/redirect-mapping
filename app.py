385428a3e2e0fffaa0cd2c710076fe9338a4f269
kevin-claneo <108477383+kevin-claneo@users.noreply.github.com>
2024-03-20 16:39:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,17 +25,17 @@ def main():
     st.markdown("""
     This tool is based on the original Python script [Automated Redirect Matchmaker for Site Migrations](https://colab.research.google.com/drive/1Y4msGtQf44IRzCotz8KMy0oawwZ2yIbT?usp=sharing) developed by [Daniel Emery](https://www.linkedin.com/in/dpe1/).
     
-    ##### Before Using the Tool 
+    #### Before Using the Tool 
     To ensure the effectiveness of this tool in mapping redirects, it is essential to adequately prepare the input data. This process begins with exporting data from *Screaming Frog*.
    
-    '🐸 Data Preparation with Screaming Frog'
+    ####🐸 Data Preparation with Screaming Frog
     
     1. Run a full crawl of your website using Screaming Frog.
     2. Filter the crawl results to include only HTML pages with a status code of 200, ensuring to remove duplicate or unnecessary URLs for redirect mapping.
     3. Export the filtered results to a CSV file. Ensure the file contains columns for the URL address, title, meta description, and other relevant information you wish to use for matching.
     4. Repeat the process for the destination website, running a crawl of the site in staging (or the new site) and exporting the results.
     
-    #####  ⚠️ Instructions for this Tool
+    ####  ⚠️ Instructions for this Tool
     
     1. Prepare the CSV files containing the URLs of the original site (`origin.csv`) and the destination site (`destination.csv`) following the instructions above.
     2. Upload the CSV files using the provided uploaders.
