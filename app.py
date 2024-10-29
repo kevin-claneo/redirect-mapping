f7bcd3f73a69a48c7fcf50bf68fee1397bb86451
kevin-claneo <108477383+kevin-claneo@users.noreply.github.com>
2024-10-29 12:59:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -57,31 +57,29 @@ def main():
     st.image("https://www.claneo.com/wp-content/uploads/Element-4.svg", width=600, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     st.caption('Adapted by [Kevin](https://www.linkedin.com/in/kirchhoff-kevin/)') 
     st.title("Use Facebook AI Similarity Search (FAISS) to find matching redirect URLs")
-    
-    st.markdown("""
-    This tool is based on the original Python script [Automated Redirect Matchmaker for Site Migrations](https://colab.research.google.com/drive/1Y4msGtQf44IRzCotz8KMy0oawwZ2yIbT?usp=sharing) developed by [Daniel Emery](https://www.linkedin.com/in/dpe1/).
-    
-    ## Before Using the Tool 
-      🚨 **Please Note:** Streamlit Cloud does not support long runtimes of scripts. For larger redirect mappings +20.000 URLs on both ends, please use Streamlit on your local machine.
-    To ensure the effectiveness of this tool in mapping redirects, it is essential to adequately prepare the input data. This process begins with exporting data from *Screaming Frog*.
-   
-    #### 🐸 Data Preparation with Screaming Frog
-    
-    1. Run a full crawl of your website using Screaming Frog.
-    2. Filter the crawl results to include only HTML pages with a status code of 200, ensuring to remove duplicate or unnecessary URLs for redirect mapping.
-    3. Export the filtered results to a CSV file. Ensure the file contains columns for the URL address, title, meta description, and other relevant information you wish to use for matching.
-    4. Repeat the process for the destination website, running a crawl of the site in staging (or the new site) and exporting the results.
-    
-    ###  ⚠️ Instructions for this Tool
-    
-    1. Prepare the CSV files containing the URLs of the original site (`origin.csv`) and the destination site (`destination.csv`) following the instructions above.
-    2. Upload the CSV files using the provided uploaders.
-    3. Select the relevant columns for matching from the dropdown menu.
-    4. Click the "Match URLs" button to start the matching process.
-    5. Download the results via "Download Results".
-    """)
-    
-    st.markdown("---")
+    with st.expander("Before using this tool",icon="🚨"):
+        st.markdown("""
+        This tool is based on the original Python script [Automated Redirect Matchmaker for Site Migrations](https://colab.research.google.com/drive/1Y4msGtQf44IRzCotz8KMy0oawwZ2yIbT?usp=sharing) developed by [Daniel Emery](https://www.linkedin.com/in/dpe1/).
+        
+        ## Before Using the Tool 
+          🚨 **Please Note:** Streamlit Cloud does not support long runtimes of scripts. For larger redirect mappings +20.000 URLs on both ends, please use Streamlit on your local machine.
+        To ensure the effectiveness of this tool in mapping redirects, it is essential to adequately prepare the input data. This process begins with exporting data from *Screaming Frog*.
+       
+        #### 🐸 Data Preparation with Screaming Frog
+        
+        1. Run a full crawl of your website using Screaming Frog.
+        2. Filter the crawl results to include only HTML pages with a status code of 200, ensuring to remove duplicate or unnecessary URLs for redirect mapping.
+        3. Export the filtered results to a CSV file. Ensure the file contains columns for the URL address, title, meta description, and other relevant information you wish to use for matching.
+        4. Repeat the process for the destination website, running a crawl of the site in staging (or the new site) and exporting the results.
+        
+        ###  ⚠️ Instructions for this Tool
+        
+        1. Prepare the CSV files containing the URLs of the original site (`origin.csv`) and the destination site (`destination.csv`) following the instructions above.
+        2. Upload the CSV files using the provided uploaders.
+        3. Select the relevant columns for matching from the dropdown menu.
+        4. Click the "Match URLs" button to start the matching process.
+        5. Download the results via "Download Results".
+        """)
 
     # Field for entering the Hugging Face token
     hf_token = str(st.secrets["installed"]["hf_token"])
